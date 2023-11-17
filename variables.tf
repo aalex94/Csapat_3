ef69be0837e9782aad5da3086ddfeb3f4904ef95
SziliCs <150517090+SziliCs@users.noreply.github.com>
2023-11-17 17:29:58 +0100

    Update variables.tf
    Writing the core of the variables.tf file.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,30 @@
+# Initializing the variables for the Terraform file
+
+# The variable file_name will be a required input!
+variable "file_name" {
+  type        = string
+  description = "The prefix of name of the <name>_local.txt file"
+}
+
+
+# The following variables won't be required inputs, they will have default valeus!
+variable "vartext" {
+  type = string
+  default = "Hello!"
+
+  validation {
+    condition     = length(var.vartext) > 5
+    error_message = "The length of the variable vartext should be more than 5!"
+  }
+}
+
+variable "varnumber" {
+  type = number
+  default = 3
+
+  validation {
+    condition     = var.varnumber <= 20
+    error_message = "The variable varnumber should be less than or equal to 20!"
+  }
+}
 
