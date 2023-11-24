c136509be72efa41c703d5e672a4d970c9b78044
eengineer96 <43788835+eengineer96@users.noreply.github.com>
2023-11-24 18:51:19 +0100

    Add files via upload
    I have made 2 new classes.
    Role.cs is an enum that defines the different kind of roles.
    Permissions.cs has a property and a field where you can save the role when you instantiate the class.

---
+++ Permissions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labor
+{
+    internal class Permissions
+    {
+        public Permissions(Role role)
+        {
+            this.Role = role;
+        }
+        
+        private Role role;
+        public Role Role
+        {
+            get { return role; }
+            private set { role = value; }
+        }
+
+
+    }
+}
