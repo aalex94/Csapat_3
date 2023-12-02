c9f65a574cfa08f1fa1ce1a5445423f7bbf9f6ab
SziliCs <150517090+SziliCs@users.noreply.github.com>
2023-12-02 09:44:45 +0100

    Update file-checker.sh
    Adding two file-checks.

--- .github/workflows/file-checker.sh
+++ .github/workflows/file-checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define the path to the file.
+# Define the path to the first file.
 FILE_PATH="/Csapat_3/Created_files/raw_local.txt"
 
 # Use an if-else statement to check if the file exists.
@@ -11,3 +11,23 @@ else
     echo "The file $FILE_PATH does not exist."
 fi
 
+
+# Check each of the files individually.
+FILE_PATH="/Csapat_3/Created_files/variables_local.txt"
+
+if test -f "$FILE_PATH"
+then
+    echo "The file $FILE_PATH exists."
+else
+    echo "The file $FILE_PATH does not exist."
+fi
+
+
+FILE_PATH="/Csapat_3/Created_files/count_0_local.txt"
+
+if test -f "$FILE_PATH"
+then
+    echo "The file $FILE_PATH exists."
+else
+    echo "The file $FILE_PATH does not exist."
+fi
