05b90c412c9fc8d2da058ea92549eb678700303a
SziliCs <150517090+SziliCs@users.noreply.github.com>
2023-12-02 09:32:03 +0100

    Update file-checker.sh
    The script for the first file-check is ready.

--- .github/workflows/file-checker.sh
+++ .github/workflows/file-checker.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Define the path to the file.
+FILE_PATH="/Csapat_3/Created_files/raw_local.txt"
+
+# Use an if-else statement to check if the file exists.
+if test -f "$FILE_PATH"
+then
+    echo "The file $FILE_PATH exists."
+else
+    echo "The file $FILE_PATH does not exist."
+fi
+
