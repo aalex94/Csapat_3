b26407599efd34b8740fa598dda6362e87597bc3
aalex94 <apjok.alex17@gmail.com>
2023-12-02 09:29:27 +0100

    Add files via upload

---
+++ Approval.cs
@@ -0,0 +1,13 @@
+Class Review
+{
+    Static void ReviewCode(string wf)
+    {
+        if(wf != "")
+        return System.Console.WriteLine("Success");
+        else{
+            System.Console.WriteLine("Error");
+        }
+    
+    }
+    
+}
---
+++ Review.cs
@@ -0,0 +1,13 @@
+Class PullRequest
+{
+    Static void Pull(string wf)
+    {
+        if(wf != "")
+        return System.Console.WriteLine("Success");
+        else{
+            System.Console.WriteLine("Error");
+        }
+    
+    }
+    
+}
